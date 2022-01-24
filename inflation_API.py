c19c1cf3edf0fdacff6c0c46f1448005370bcdca
fndpires <fndpires@gmail.com>
2022-01-24 15:58:47 -0300

    Inflation Analysis notebook

---
+++ inflation_API.py
@@ -0,0 +1,51 @@
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+model = pickle.load(open("model_ipca.pkl", "rb"))
+
+class Factors(BaseModel):
+    food: float
+    housing: float
+    house_items: float
+    clothing: float
+    transport: float 
+    communication: float 
+    health: float
+    personal: float
+    education: float
+    int_rate: float
+    min_wages: float
+    
+    
+@app.post("/inflation")   
+async def predict_inflation(factor: Factors):
+    data = factor.dict()
+    data_in = [[data["food"], data["housing"], data["house_items"], data["clothing"],
+    data["transport"], data["communication"], data["health"], data["personal"],
+    data["education"], data["int_rate"], data["min_wages"]]]
+    
+    prediction = model.predict(data_in)
+     
+    return {"prediction": prediction[0]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+ # = ['food', 'housing', 'house_items', 'clothing', 
+                        #'transport', 'communication', 'health', 'personal', 
+                           #'education', 'int_rate', 'min_wages']
